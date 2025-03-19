a8317e64afee272623a27cd960b3f7a16eb8b616
mateuszrosa <mateusz.rosa@gmail.com>
2025-03-19 11:18:28 +0100

    again

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./styles.scss";
+
+function App() {
+
+  return (
+    <div className="container">
+      <header>SHOPPING LIST</header>
+      <div className="form">
+        <form action="">
+          <input type="text" name="" id="" placeholder="Write your porduct" />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+      <div className="shopping-list"></div>
+    </div>
+  )
+}
+
+export default App
