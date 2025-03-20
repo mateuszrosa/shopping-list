273d505651320f1bcaf0043aa7b967411ee36c98
mateuszrosa <mateusz.rosa@gmail.com>
2025-03-20 17:20:21 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import "./styles.scss";
 
+
 function App() {
 
   return (
     <div className="container">
-      <header>SHOPPING LIST</header>
-      <div className="form">
-        <form action="">
-          <input type="text" name="" id="" placeholder="Write your porduct" />
-          <button type="submit">Add</button>
-        </form>
-      </div>
-      <div className="shopping-list"></div>
+      <header>
+        <h3>Shopping List</h3>
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4ZM3 6h18"></path><path d="M16 10a4 4 0 0 1-8 0"></path></g></svg>
+      </header>
     </div>
   )
 }
